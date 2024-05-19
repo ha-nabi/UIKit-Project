46f6a277e57734e5c0322a4aabc2608bd8a98d53
kangciu <a2849535@gmail.com>
2024-05-19 19:44:54 +0900

    Initial Commit

---
+++ Neflix/Controllers/DownloadsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DownloadsViewController.swift
+//  Neflix
+//
+//  Created by 강치우 on 5/19/24.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

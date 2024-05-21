d37f1678a495954cf593cd7b5c0d0b41b60b8d53
kangciu <a2849535@gmail.com>
2024-05-21 15:23:27 +0900

    feat; API 모델 추가

---
+++ Neflix/Managers/APICaller.swift
@@ -0,0 +1,38 @@
+//
+//  APICaller.swift
+//  Neflix
+//
+//  Created by 강치우 on 5/21/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "697d439ac993538da4e3e60b54e762cd"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failTogetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
---
+++ Neflix/Model/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  Neflix
+//
+//  Created by 강치우 on 5/21/24.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
